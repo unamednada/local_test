e64cfb1fae52b076a866460b5cbd500e20723618
unamednada <dias.gustavo.dias@gmail.com>
2022-04-06 18:42:20 -0300

    Add describe and it to header.test

--- tests/Header.test.js
+++ tests/Header.test.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Header from '../src/components';
 
-test('renders learn react link', () => {
-  render(<Header />);
-  const sourceLink = screen.getByText(/see the source code here/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Tests Component Header', () => {
+  it('renders a link to the source code', () => {
+    render(<Header />);
+    const sourceLink = screen.getByText(/see the source code here/i);
+    expect(sourceLink).toBeInTheDocument();
+  });
 });
