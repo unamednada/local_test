342150b4d16b36bf353c4435486cb6ef8e130504
unamednada <dias.gustavo.dias@gmail.com>
2022-04-07 20:58:46 -0300

    Render header in app and refactor it to be a functional component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 
-export default class Header extends React.Component {
-  render() {
-    return (
-      <div className="header">
-        <div className="header-content">
-          <div className="header-content-inner">
-            <h1>{this.props.title}</h1>
-          </div>
+export default function Header({ title }) {
+  return (
+    <div className="header">
+      <div className="header-content">
+        <div className="header-content-inner">
+          <h1>{title}</h1>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
