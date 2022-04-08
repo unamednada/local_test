444346e4ed43c6efc39a28cc10d971f1fd28017f
unamednada <dias.gustavo.dias@gmail.com>
2022-04-08 17:36:18 -0300

    Add target to source code link test and create test for nav. 6 failed

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -26,5 +26,48 @@ describe('Tests Header Component', () => {
     const repoLink = leftSection.children[0];
     expect(repoLink).toHaveAttribute('href', 'https://github.com/unamednada/unamednada.github.io');
     expect(repoLink).toHaveTextContent('see source code here');
+    expect(repoLink).toHaveAttribute('target', '_blank');
+  });
+
+  it('should have a navigation bar in the right section', () => {
+    const headerContainer = screen.getByTestId('header');
+    const [, rightSection] = headerContainer.children;
+    const navBar = rightSection.children[0];
+    expect(navBar).toHaveClass('header-nav');
+  });
+
+  it('should have navigation links in the nav bar', () => {
+    const headerContainer = screen.getByTestId('header');
+    const [, rightSection] = headerContainer.children;
+    const navBar = rightSection.children[0];
+    expect(navBar.children.length).toBe(3);
+    const [aboutLink, projectsLink, homeLink] = navBar.children;
+    expect(aboutLink).toHaveTextContent('about');
+    expect(projectsLink).toHaveTextContent('projects');
+    expect(homeLink).toHaveTextContent('home');
+  });
+
+  it('should have a link to the about page in the nav bar', () => {
+    const headerContainer = screen.getByTestId('header');
+    const [, rightSection] = headerContainer.children;
+    const navBar = rightSection.children[0];
+    const aboutLink = navBar.children[0];
+    expect(aboutLink).toHaveAttribute('href', '/about');
+  });
+
+  it('should have a link to the projects page in the nav bar', () => {
+    const headerContainer = screen.getByTestId('header');
+    const [, rightSection] = headerContainer.children;
+    const navBar = rightSection.children[0];
+    const projectsLink = navBar.children[1];
+    expect(projectsLink).toHaveAttribute('href', '/projects');
+  });
+
+  it('should have a link to the home page in the nav bar', () => {
+    const headerContainer = screen.getByTestId('header');
+    const [, rightSection] = headerContainer.children;
+    const navBar = rightSection.children[0];
+    const homeLink = navBar.children[2];
+    expect(homeLink).toHaveAttribute('href', '/');
   });
 });
