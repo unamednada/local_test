de123cca22957b766f2527024b79a416fcf5724d
unamednada <dias.gustavo.dias@gmail.com>
2022-04-08 17:44:04 -0300

    Refactor tests to use nested describes for organization

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -2,72 +2,63 @@ import { render, screen } from '@testing-library/react';
 import { Header } from '../components';
 
 describe('Tests Header Component', () => {
-
-  beforeEach(() => {
-    render(<Header />);
-  });
+  render(<Header />);
+  const headerContainer = screen.getByTestId('header');
 
   it('should render the header', () => {
-    const headerContainer = screen.getByTestId('header');
     expect(headerContainer).toBeInTheDocument();
   });
 
   it('should have a left and right section', () => {
-    const headerContainer = screen.getByTestId('header');
     expect(headerContainer.children.length).toBe(2);
     const [leftSection, rightSection] = headerContainer.children;
     expect(leftSection).toHaveClass('header-left');
     expect(rightSection).toHaveClass('header-right');
   });
 
-  it('should have a link to the github repo in the left section', () => {
-    const headerContainer = screen.getByTestId('header');
-    const [leftSection] = headerContainer.children;
-    const repoLink = leftSection.children[0];
-    expect(repoLink).toHaveAttribute('href', 'https://github.com/unamednada/unamednada.github.io');
-    expect(repoLink).toHaveTextContent('see source code here');
-    expect(repoLink).toHaveAttribute('target', '_blank');
+  describe('The left section', () => {
+    it('should have a link to the github repo', () => {
+      const [leftSection] = headerContainer.children;
+      const repoLink = leftSection.children[0];
+      expect(repoLink).toHaveAttribute('href', 'https://github.com/unamednada/unamednada.github.io');
+      expect(repoLink).toHaveTextContent('see source code here');
+      expect(repoLink).toHaveAttribute('target', '_blank');
+    });
   });
 
-  it('should have a navigation bar in the right section', () => {
-    const headerContainer = screen.getByTestId('header');
+  describe('The right section', () => {
     const [, rightSection] = headerContainer.children;
-    const navBar = rightSection.children[0];
-    expect(navBar).toHaveClass('header-nav');
-  });
 
-  it('should have navigation links in the nav bar', () => {
-    const headerContainer = screen.getByTestId('header');
-    const [, rightSection] = headerContainer.children;
-    const navBar = rightSection.children[0];
-    expect(navBar.children.length).toBe(3);
-    const [aboutLink, projectsLink, homeLink] = navBar.children;
-    expect(aboutLink).toHaveTextContent('about');
-    expect(projectsLink).toHaveTextContent('projects');
-    expect(homeLink).toHaveTextContent('home');
-  });
+    it('should have a navigation bar', () => {
+      const navBar = rightSection.children[0];
+      expect(navBar).toHaveClass('header-nav');
+    });
 
-  it('should have a link to the about page in the nav bar', () => {
-    const headerContainer = screen.getByTestId('header');
-    const [, rightSection] = headerContainer.children;
-    const navBar = rightSection.children[0];
-    const aboutLink = navBar.children[0];
-    expect(aboutLink).toHaveAttribute('href', '/about');
-  });
+    describe('The navigation bar', () => {
+      const navBar = rightSection.children[0];
 
-  it('should have a link to the projects page in the nav bar', () => {
-    const headerContainer = screen.getByTestId('header');
-    const [, rightSection] = headerContainer.children;
-    const navBar = rightSection.children[0];
-    const projectsLink = navBar.children[1];
-    expect(projectsLink).toHaveAttribute('href', '/projects');
-  });
-
-  it('should have a link to the home page in the nav bar', () => {
-    const headerContainer = screen.getByTestId('header');
-    const [, rightSection] = headerContainer.children;
-    const navBar = rightSection.children[0];
-    const homeLink = navBar.children[2];
-    expect(homeLink).toHaveAttribute('href', '/');
+      it('should have navigation links', () => {
+        expect(navBar.children.length).toBe(3);
+        const [aboutLink, projectsLink, homeLink] = navBar.children;
+        expect(aboutLink).toHaveTextContent('about');
+        expect(projectsLink).toHaveTextContent('projects');
+        expect(homeLink).toHaveTextContent('home');
+      });
+    
+      it('should have a link to the about page', () => {
+        const aboutLink = navBar.children[0];
+        expect(aboutLink).toHaveAttribute('href', '/about');
+      });
+    
+      it('should have a link to the projects page', () => {
+        const projectsLink = navBar.children[1];
+        expect(projectsLink).toHaveAttribute('href', '/projects');
+      });
+    
+      it('should have a link to the home page', () => {
+        const homeLink = navBar.children[2];
+        expect(homeLink).toHaveAttribute('href', '/');
+      });
+    });
   });
 });
