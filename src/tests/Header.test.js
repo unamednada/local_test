940e19703f9fcd3c34000d122818843895a36bb0
unamednada <dias.gustavo.dias@gmail.com>
2022-04-08 17:27:05 -0300

    Create test for link

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -2,18 +2,29 @@ import { render, screen } from '@testing-library/react';
 import { Header } from '../components';
 
 describe('Tests Header Component', () => {
-  it('should render the header', () => {
+
+  beforeEach(() => {
     render(<Header />);
+  });
+
+  it('should render the header', () => {
     const headerContainer = screen.getByTestId('header');
     expect(headerContainer).toBeInTheDocument();
   });
 
   it('should have a left and right section', () => {
-    render(<Header />);
     const headerContainer = screen.getByTestId('header');
     expect(headerContainer.children.length).toBe(2);
     const [leftSection, rightSection] = headerContainer.children;
     expect(leftSection).toHaveClass('header-left');
     expect(rightSection).toHaveClass('header-right');
+  });
+
+  it('should have a link to the github repo in the left section', () => {
+    const headerContainer = screen.getByTestId('header');
+    const [leftSection] = headerContainer.children;
+    const repoLink = leftSection.children[0];
+    expect(repoLink).toHaveAttribute('href', 'https://github.com/unamednada/unamednada.github.io');
+    expect(repoLink).toHaveTextContent('see source code here');
   });
 });
