1fdae945a0e81ef0cdc0d0bc2bc6443cb22b00c2
unamednada <dias.gustavo.dias@gmail.com>
2022-04-08 17:11:20 -0300

    Create test for header children.

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -7,4 +7,13 @@ describe('Tests Header Component', () => {
     const headerContainer = screen.getByTestId('header');
     expect(headerContainer).toBeInTheDocument();
   });
+
+  it('should have a left and right section', () => {
+    render(<Header />);
+    const headerContainer = screen.getByTestId('header');
+    expect(headerContainer.children.length).toBe(2);
+    const [leftSection, rightSection] = headerContainer.children;
+    expect(leftSection).toHaveClass('header-left');
+    expect(rightSection).toHaveClass('header-right');
+  });
 });
