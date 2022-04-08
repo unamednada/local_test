da2fe53e109e9c69b3b8e6de2dc57b2f048cf82f
unamednada <dias.gustavo.dias@gmail.com>
2022-04-07 21:02:52 -0300

    Implement minimum to pass test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,17 @@ export default function Header({ title }) {
   return (
     <div className="header">
       <div className="header-content">
-        <div className="header-content-inner">
+        <div className="header-content-left">
           <h1>{title}</h1>
+        </div>
+        <div className="header-content-right">
+          <nav>
+            <ul>
+              <li><a>home</a></li>
+              <li><a>projects</a></li>
+              <li><a>about</a></li>
+            </ul>
+          </nav>
         </div>
       </div>
     </div>
