201ae1fed151aba1853eb845dac99f24f4f43c82
unamednada <dias.gustavo.dias@gmail.com>
2022-04-08 18:00:27 -0300

    Create test for app

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('The main application', () => {
+  it('should render the header', () => {
+    render(<App />);
+    const header = screen.getByTestId('header');
+    expect(header).toBeInTheDocument();
+  });
 });
