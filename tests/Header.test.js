68eedaae9363635c6742305fec9793703307cbf3
unamednada <dias.gustavo.dias@gmail.com>
2022-04-07 21:01:00 -0300

    Create test for nav and fail. need implement

--- tests/Header.test.js
+++ tests/Header.test.js
@@ -7,10 +7,16 @@ import { render, screen } from '@testing-library/react';
 import { Header } from '../src/components';
 
 describe('Tests Component Header', () => {
+  const title = 'see the source code here';
   it('renders a link to the source code', () => {
-    const title = 'see the source code here';
     render(<Header title={title} />);
     const sourceLink = screen.getByText(/see the source code here/i);
     expect(sourceLink).toBeInTheDocument();
+  });
+
+  it('renders a navigation bar', () => {
+    render(<Header title={title} />);
+    const nav = screen.getByRole('navigation');
+    expect(nav).toBeInTheDocument();
   });
 });
