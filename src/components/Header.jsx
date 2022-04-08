9f29d0bcbd9bd11221979861852acc5c2def5e33
unamednada <dias.gustavo.dias@gmail.com>
2022-04-08 20:50:22 -0300

    All fixed. no bugs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,24 @@
-import React from 'react';
-
 function Header() {
   return (
-    <header data-testid='header'>
-      <div className='header-left'>
+    <header data-testid="header">
+      <div className="header-left">
         <a
-          href='https://github.com/unamednada/unamednada.github.io'
-          target='_blank'
+          href="https://github.com/unamednada/unamednada.github.io"
+          target="_blank"
+          rel="noreferrer"
         >
           see source code here
         </a>
       </div>
-      <div className='header-right'>
-        <nav className='header-nav'>
-          <a href='/'>home</a>
-          <a href='/projects'>projects</a>
-          <a href='/about'>about</a>
+      <div className="header-right">
+        <nav className="header-nav">
+          <a href="/">home</a>
+          <a href="/projects">projects</a>
+          <a href="/about">about</a>
         </nav>
       </div>
     </header>
-  )
+  );
 }
 
 export default Header;
