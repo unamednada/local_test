a2d86df601304b21d7e4e8b09ee72f03b766e067
unamednada <dias.gustavo.dias@gmail.com>
2022-05-24 16:29:01 -0300

    Create stack component and migrate banner to templates folder. page ok

---
+++ src/components/Stack/index.jsx
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+import './Stack.css';
+
+function Stack({ icons, content }) {
+  return (
+    <div className={ `banner-stack-icons ${content}` }>
+      {icons.map((icon, index) => (
+        <img key={ index } src={ icon === 'chai' ? './images/chai.svg' : `./images/${icon}.png` } alt={ `icon-${icon}` } />
+      ))}
+      <div className="stack-content">{ `${content.toUpperCase()}` }</div>
+    </div>
+  );
+}
+
+Stack.propTypes = {
+  icons: PropTypes.arrayOf(PropTypes.string).isRequired,
+  content: PropTypes.string.isRequired,
+};
+
+export default Stack;
---
+++ src/service/mockStackDB.js
@@ -0,0 +1,38 @@
+const frontEnd = [
+  'html5',
+  'css3',
+  'javascript',
+  'react',
+  'redux',
+];
+
+const backEnd = [
+  'node',
+  'express',
+  'typescript',
+  'sequelize',
+  'mysql',
+];
+
+const testing = [
+  'jest',
+  'mocha',
+  'chai',
+  'sinon',
+  'rtl',
+];
+
+const cloud = [
+  'heroku',
+  'docker',
+  'supabase',
+  'github-pages',
+  'github-actions',
+];
+
+export {
+  frontEnd,
+  backEnd,
+  testing,
+  cloud,
+};
