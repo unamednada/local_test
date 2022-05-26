f57f2ea9e37173149719153a09ba7d2208b0f10d
unamednada <dias.gustavo.dias@gmail.com>
2022-05-25 22:02:27 -0300

    Fix style of spotlight in projects

--- src/templates/Showroom/index.jsx
+++ src/templates/Showroom/index.jsx
@@ -6,14 +6,12 @@ function Showroom() {
   return (
     <div className="showroom" data-testid="showroom">
       <div className="showroom-main">
-        <div className="showroom-main-left">
-          <Spotlight
-            title={ projects.title }
-            className={ projects.className }
-          >
-            { projects.children }
-          </Spotlight>
-        </div>
+        <Spotlight
+          title={ projects.title }
+          className={ projects.className }
+        >
+          { projects.children }
+        </Spotlight>
       </div>
     </div>
   );
