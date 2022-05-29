7131ea31a7f4fad23f52ee1fa18be2d1e162f737
unamednada <dias.gustavo.dias@gmail.com>
2022-05-29 11:55:28 -0300

    Create dotenv file and add it to emailConnection

---
+++ src/service/emailConnection.js
@@ -0,0 +1,28 @@
+require('dotenv');
+const mailjet = require ('node-mailjet')
+.connect(process.env.MAIL_TOKEN, process.env.MAIL_KEY);
+
+const request = async (email, message) => await mailjet
+.post("send", {'version': 'v3.1'})
+.request({
+  "Messages":[
+    {
+      "From": {
+        "Email": email,
+        "Name": "Visitor"
+      },
+      "To": [
+        {
+          "Email": "dias.gustavo.dias@gmail.com",
+          "Name": "Gustavo",
+        },
+      ],
+      "Subject": "Greetings from visitor",
+      "TextPart": message,
+      "HTMLPart": "",
+      "CustomID": `${email}-${Date.now().toLocaleString()}`,
+    },
+  ],
+});
+
+export default request;
