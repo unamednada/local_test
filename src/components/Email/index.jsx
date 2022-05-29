05b50163ef35f0eb5a312e0d39d001dcb6c7b430
unamednada <dias.gustavo.dias@gmail.com>
2022-05-29 12:05:43 -0300

    Make email and message controlled components and add submit generic handler

--- src/components/Email/index.jsx
+++ src/components/Email/index.jsx
@@ -1,11 +1,33 @@
 import './Email.css';
+import { useState } from 'react';
 
 function Email() {
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const submit = (event) => {
+    event.preventDefault();
+    console.log(email, message);
+  };
+
   return (
     <div className="email-main">
-      <form>
-        <input type="email" name="email" id="email" placeholder="Email" />
-        <textarea name="message" id="message" placeholder="Message" />
+      <form onSubmit={ submit }>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Email"
+          value={ email }
+          onChange={ ({ target: { value } }) => setEmail(value) }
+        />
+        <textarea
+          name="message"
+          id="message"
+          placeholder="Message"
+          value={ message }
+          onChange={ ({ target: { value } }) => setMessage(value) }
+        />
         <button type="submit">Send</button>
       </form>
     </div>
