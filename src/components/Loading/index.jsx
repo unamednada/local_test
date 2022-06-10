d6503e7bce54e490962d5c4bf495af22b0fddd13
unamednada <dias.gustavo.dias@gmail.com>
2022-06-09 21:01:59 -0300

    Create generic loading component

---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,13 @@
+import './Loading.css';
+
+function Loading() {
+  return (
+    <div className="loading">
+      <div className="loading-message">
+        <p>Loading...</p>
+      </div>
+    </div>
+  );
+}
+
+export default Loading;
