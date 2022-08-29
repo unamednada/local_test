24f842132dc1398ebcef173cee2a83acb21e2778
unamednada <dias.gustavo.dias@gmail.com>
2022-08-29 16:07:58 -0300

    return: remove headers

--- src/utils/APIRequest.js
+++ src/utils/APIRequest.js
@@ -4,13 +4,7 @@ const { REACT_APP_HOSTNAME } = process.env;
 
 const baseURL = `${REACT_APP_HOSTNAME}`;
 
-const headers = {
-  'Content-Type': 'application/json',
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-};
-
-const api = axios.create({ baseURL, headers });
+const api = axios.create({ baseURL });
 
 export const getData = async (endpoint) => api.get(endpoint);
 
