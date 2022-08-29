62be376df8d2bd0953aaeb0746170afc62815119
unamednada <dias.gustavo.dias@gmail.com>
2022-08-29 16:19:47 -0300

    return: remove generic uri. changes in backend

--- src/utils/APIRequest.js
+++ src/utils/APIRequest.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const { REACT_APP_HOSTNAME } = process.env;
 
-const baseURL = `https://cors-anywhere.herokuapp.com/${REACT_APP_HOSTNAME}`;
+const baseURL = `${REACT_APP_HOSTNAME}`;
 
 const api = axios.create({ baseURL });
 
