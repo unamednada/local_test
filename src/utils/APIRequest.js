5c648df97e9ad5dcbf2389a7857d07e5ddac33a1
unamednada <dias.gustavo.dias@gmail.com>
2022-08-29 16:02:40 -0300

    fix: add headers to request properly

--- src/utils/APIRequest.js
+++ src/utils/APIRequest.js
@@ -4,7 +4,13 @@ const { REACT_APP_HOSTNAME } = process.env;
 
 const baseURL = `${REACT_APP_HOSTNAME}`;
 
-const api = axios.create({ baseURL, headers: { 'Access-Control-Allow-Origin': '*' } });
+const headers = {
+  'Content-Type': 'application/json',
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+};
+
+const api = axios.create({ baseURL, headers });
 
 export const getData = async (endpoint) => api.get(endpoint);
 
